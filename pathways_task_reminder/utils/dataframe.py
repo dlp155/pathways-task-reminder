3ef1c5e4959a07f34abf2238f480267abf92027d
John T. Prince <jtprince@gmail.com>
2024-12-25 18:37:08 -0700

    some styling

--- pathways_task_reminder/utils/dataframe.py
+++ pathways_task_reminder/utils/dataframe.py
@@ -13,7 +13,7 @@ def to_table(df: pd.DataFrame, format: str = "html"):
 
     display_df = df.where(pd.notna(df), None)
 
-    first_header = df.columns.name or df.index.name or None
+    first_header = df.columns.name or df.index.name or ""
     header_row = [first_header, *display_df.columns.tolist()]
     rows = [[index] + row.tolist() for index, row in display_df.iterrows()]
 
@@ -23,4 +23,20 @@ def to_table(df: pd.DataFrame, format: str = "html"):
         tablefmt="html",
         # numalign="right"
     )
-    return tabulate_data
+
+    style_block = """<style>
+    table, th, td {
+        border: 1px solid #d3d3d3;
+        border-collapse: collapse;
+    }
+    th {
+        text-align: center;
+        padding: 6px;
+        background-color: #f9f9f9;
+    }
+    td {
+        text-align: left;
+        padding: 6px;
+    }
+    </style>\n"""
+    return style_block + tabulate_data
